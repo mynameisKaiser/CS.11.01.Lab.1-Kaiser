033519d253f87063440b75442b00f02f75060de6
mynameisKaiser <kaisertsoi@hotmail.com>
2024-08-25 09:12:11 +0800

    Task 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,11 @@
-public class Dateable {
+import java.util.Scanner;  // Import the Scanner class
 
+class Dateable {
     public static void main(String[] args) {
-        
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("How old are you? ");
+        String userAge = myObj.nextLine();  // Read user input
+        int userAgeInt = Integer.parseInt(userAge);
+        System.out.println("You should date someone who is at least  " + (7+(userAgeInt/2)));  // Output user input
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
-public class Pages {
+import java.util.Scanner;  // Import the Scanner class
 
+class Pages {
     public static void main(String[] args) {
-
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("How old are you? ");
+        String userAge = myObj.nextLine();  // Read user input
+        int userAgeInt = Integer.parseInt(userAge);
+        System.out.println(userAgeInt+"'s should read at least " +(100-userAgeInt)+" pages before giving up on a book.");  // Output user input
     }
 }
